9d2d4bbdce121d48e63b58a92034cf348ecd69c2
Shahmar700 <shahmar700@gmail.com>
2025-05-08 23:33:15 +0400

    add services page

--- pages/hizmetler.vue
+++ pages/hizmetler.vue
@@ -7,6 +7,64 @@
           Elton Teknik Servis olarak, bilgisayar, telefon ve tablet tamiri konusunda uzmanlaşmış bir ekip olarak müşterilerimize eşsiz hizmetler sunuyoruz. Geniş marka yelpazemiz ve kapsamlı arıza türleriyle her türlü cihaz sorununu çözme konusunda deneyimliyiz.
         </p>
       </div>
+      <!-- pc and phone repair  -->
+       <div style="width: 100%">
+         <div class="pc-repair-container" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="100">
+             <div class="pc-repair-icon">
+                <Icon name="hugeicons:settings-02" class="icon settings-icon" />
+                <img src="/public/assets/icons/laptop.png" alt="phone" class="icon laptop-icon">
+                <!-- <Icon name="material-symbols:laptop-mac-outline-sharp" class="icon laptop-icon" />  -->
+             </div>
+            <h3 class="common-repair-title">Bilgisayar Tamiri</h3>
+         </div>
+         <!-- Laptop Repair  -->
+          <div class="info-text" data-aos="fade-up" data-aos-duration="900" data-aos-delay="200">
+              <div class="repair-heading">
+                <Icon name="hugeicons:repair" class="icon repair-icon" />  
+                <h4>Laptop Tamiri</h4>
+              </div>
+              <p class="">Pek çok markanın laptop tamirini gerçekleştiriyoruz. Ekran değişimi, klavye tamiri, batarya değişimi, anakart onarımı ve yazılım sorunları gibi birçok hizmeti sunuyoruz.</p>
+          </div>
+
+          <!-- Desktop Repair  -->
+          <div class="info-text" data-aos="fade-up" data-aos-duration="800" data-aos-delay="200">
+              <div class="repair-heading">
+                <Icon name="hugeicons:repair" class="icon repair-icon" />  
+                <h4>Masaüstü Bilgisayarlar Tamiri</h4>
+              </div>
+              <p class="">Tüm markaların modellerin masaüstü bilgisayarlarının tamirini yapıyoruz. Güç kaynağı değişimi, donanım yükseltme, veri kurtarma ve virüs temizleme gibi hizmetlerle cihazlarınızı yeniden çalışır duruma getiriyoruz.</p>
+          </div>
+
+          <div class="pc-repair-container" data-aos="fade-up" data-aos-duration="1000">
+             <div class="pc-repair-icon">
+                <Icon name="hugeicons:settings-02" class="icon settings-icon" />
+                 <img src="/public/assets/icons/phone.png" alt="phone" class="icon laptop-icon">
+             </div>
+            <h3 class="common-repair-title">Telefon Tamiri</h3>
+         </div>
+
+         <!-- phone repair  -->
+         <div class="info-text" data-aos="fade-up" data-aos-duration="1000">
+              <div class="repair-heading">
+                <Icon name="hugeicons:repair" class="icon repair-icon" />  
+                <h4>Telefon Tamiri</h4>
+              </div>
+              <p class="">Tüm telefon modellerinin ekran değişimi, batarya değişimi, arka kamera tamiri, şarj portu onarımı gibi hizmetleri sunuyoruz. Tüm marka modellerin tamirini gerçekleştiriyoruz.</p>
+          </div>
+
+          <!-- tablet repair  -->
+          <div class="info-text" data-aos="fade-up" data-aos-duration="1000">
+              <div class="repair-heading">
+                <Icon name="hugeicons:repair" class="icon repair-icon" />  
+                <h4>Tablet Tamiri</h4>
+              </div>
+              <p class="">Tüm marka modellerinin ekran değişimi, batarya değişimi, güç düğmesi tamiri, WiFi sorunları gibi hizmetlerini sunuyoruz.</p>
+          </div>
+
+          <div class="info-text" data-aos="fade-up" data-aos-duration="1000">
+            <p>Bu sadece bir örnek olup, daha fazla marka ve arıza türü için bizimle iletişime geçebilirsiniz. Elton Teknik Servis olarak, uzman teknisyenlerimizle en kaliteli hizmeti sunmayı hedefliyoruz. Her türlü tamir ihtiyacınız için bize güvenebilirsiniz.</p>
+          </div>
+       </div>
     </section>
   </div>
 </template>
@@ -24,6 +82,68 @@ section {
   flex-direction: column;
   align-items: center;
   justify-content: flex-start;
+}
+.common-repair-title {
+  font-size: 2rem;
+  font-weight: 700;
+  color: #1AA54D;
+}
+.pc-repair-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 3rem;
+}
+.icon {
+  font-size: 3rem;
+}
+.pc-repair-icon {
+  position: relative;
+  margin-right: 10px;
+}
+.settings-icon {
+  color: #1AA54D;
+  position: absolute;
+  top: -7px;
+  left: -8px;
+  font-size: 2rem;
+  animation: rotate 7s linear infinite;
+  z-index: -1;
+}
+.laptop-icon {
+  color: #1AA54D;
+  width: 50px;
+  height: 50px;
+  z-index: 2;
+}
+
+@keyframes rotate {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+.repair-heading {
+  width: fit-content;
+  display: flex;
+  align-items: center;
+  border: 1px solid #1AA54D;
+  padding: 5px 15px;
+  border-radius: 10px;
+  background: linear-gradient(45deg, #1AA54D, #121D61);
+  color: white;
+}
+.repair-icon {
+  font-size: 2rem;
+  color: #fff;
+  margin-right: 10px;
+}
+.repair-heading h4 {
+  font-size: 1.5rem;
+  font-weight: 700;
+  color: #fff;
 }
 
 .info-text {
