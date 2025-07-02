4b606347eb808de52eb829134f4fe7cc0006e927
Shahmar700 <shahmar700@gmail.com>
2025-07-02 17:56:11 +0400

    add title name

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -31,7 +31,7 @@ export default defineNuxtConfig({
   app: {
     pageTransition: { name: 'page', mode: 'out-in' },
     head: {
-      title: 'Elton',
+      title: 'Elton Teknik Servis',
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' }
